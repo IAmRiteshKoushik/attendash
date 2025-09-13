54d505d7a734f4ae0c8550756f7f17f793e73a0a
Nandgopal-R <nandgopalrnair@gmail.com>
2025-09-13 23:23:19 +0530

    feat: add file picker

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.3
 
 require (
 	github.com/appwrite/sdk-for-go v0.11.0
+	github.com/charmbracelet/bubbles v0.21.0
 	github.com/charmbracelet/bubbletea v1.3.6
 	github.com/charmbracelet/lipgloss v1.1.0
 	github.com/charmbracelet/log v0.4.2
@@ -19,6 +20,7 @@ require (
 	github.com/charmbracelet/x/ansi v0.9.3 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
--- ui/components/file_explorer.go
+++ ui/components/file_explorer.go
@@ -3,3 +3,125 @@ package components
 // Component for finding the CSV file which contains an "export" of all the form
 // data. We need to read it, pick out the relevant fields and then populate
 // AppWrite.
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/bubbles/filepicker"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type FileSelectedMsg struct {
+	Path string
+}
+
+type FilePickerModel struct {
+	filepicker   filepicker.Model
+	selectedFile string
+	quitting     bool
+	err          error
+}
+
+type clearErrorMsg struct{}
+
+func clearErrorAfter(t time.Duration) tea.Cmd {
+	return tea.Tick(t, func(_ time.Time) tea.Msg {
+		return clearErrorMsg{}
+	})
+}
+
+func NewFilePicker() FilePickerModel {
+	fp := filepicker.New()
+	fp.AllowedTypes = []string{".csv"}
+	fp.CurrentDirectory, _ = os.UserHomeDir()
+	fp.ShowHidden = true
+	fp.DirAllowed = true
+
+	// fp.Height = 20
+	fp.SetHeight(20)
+
+	return FilePickerModel{
+		filepicker: fp,
+	}
+}
+
+func NewFilePickerModel() FilePickerModel {
+	return NewFilePicker()
+}
+
+func (m FilePickerModel) Init() tea.Cmd {
+	return m.filepicker.Init()
+}
+
+func (m FilePickerModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+		}
+	case clearErrorMsg:
+		m.err = nil
+	}
+
+	var cmd tea.Cmd
+	m.filepicker, cmd = m.filepicker.Update(msg)
+
+	// Did the user select a file?
+	if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
+		// Get the path of the selected file.
+		m.selectedFile = path
+		selectedCmd := func() tea.Msg {
+			return FileSelectedMsg{Path: path}
+		}
+
+		return m, tea.Batch(cmd, selectedCmd)
+	}
+
+	// Did the user select a disabled file?
+	// This is only necessary to display an error to the user.
+	if didSelect, path := m.filepicker.DidSelectDisabledFile(msg); didSelect {
+		// Let's clear the selectedFile and display an error.
+		m.err = errors.New(path + " is not valid.")
+		m.selectedFile = ""
+		return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
+	}
+
+	return m, cmd
+}
+
+func (m FilePickerModel) View() string {
+	if m.quitting {
+		return ""
+	}
+	var s strings.Builder
+	s.WriteString("\n  ")
+	if m.err != nil {
+		s.WriteString(m.filepicker.Styles.DisabledFile.Render(m.err.Error()))
+	} else if m.selectedFile == "" {
+		s.WriteString("Pick a file:")
+	} else {
+		s.WriteString("Selected file: " + m.filepicker.Styles.Selected.Render(m.selectedFile))
+	}
+	s.WriteString("\n\n" + m.filepicker.View() + "\n")
+	return s.String()
+}
+
+func main() {
+	fp := filepicker.New()
+	// fp.AllowedTypes = []string{".mod", ".sum", ".go", ".txt", ".md"}
+	fp.AllowedTypes = []string{".csv"}
+	fp.CurrentDirectory, _ = os.UserHomeDir()
+
+	m := FilePickerModel{
+		filepicker: fp,
+	}
+	tm, _ := tea.NewProgram(&m).Run()
+	mm := tm.(FilePickerModel)
+	fmt.Println("\n  You selected: " + m.filepicker.Styles.Selected.Render(mm.selectedFile) + "\n")
+}
