d2dd386d772fd33844a1a25af5d2e13b5414fe95
Nandgopal-R <nandgopalrnair@gmail.com>
2025-09-16 02:25:34 +0530

    add function to insert event to db

--- cmd/event.go
+++ cmd/event.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"github.com/IAmRiteshKoushik/attendash/api"
 	"github.com/IAmRiteshKoushik/attendash/forms"
+	"github.com/appwrite/sdk-for-go/id"
+	"github.com/appwrite/sdk-for-go/models"
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 )
@@ -25,4 +27,26 @@ func launchEventForm(cmd *cobra.Command, args []string) {
 	if err := form.Run(); err != nil {
 		log.Error(err)
 	}
+}
+
+func CreateEvent(e api.Event) (*models.Row, error) {
+	eventDate := map[string]interface{}{
+		"eventName":     e.Name,
+		"isOffline":     e.IsOffline,
+		"eventLocation": e.Location,
+		"dateAndTime":   e.Datetime,
+		"label":         e.Label,
+	}
+
+	doc, err := Orm.CreateRow(
+		dbName,
+		EventsTable.Id,
+		id.Unique(),
+		eventDate,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
 }
